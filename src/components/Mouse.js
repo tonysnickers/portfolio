b7a96e74238e73d5018a1c92ab38024c0995a11c
tonysnickers <antoine.mendy@hotmail.fr>
2022-10-24 17:04:24 +0200

    mouse move created

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Mouse = () => {
+
+  useEffect(() => {
+    const cursor = document.querySelector(".cursor")
+
+    window.addEventListener("mousemove", (e) => {
+      cursor.style.top = e.y + "px"
+      cursor.style.left = e.x + "px"
+    })
+
+    document.querySelectorAll(".hover").forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        cursor.classList.add("hovered")
+      })
+      link.addEventListener("mouseleave", () => {
+        cursor.classList.remove("hovered")
+
+      })
+    })
+  })
   return (
-    <div>Mouse</div>
+
+    <span className="cursor">
+
+    </span>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Mouse from '../components/Mouse'
 import Navigation from '../components/Navigation'
 
 const Home = () => {
   return (
     <div>
+      <Mouse/>
       <Navigation />
     </div>
   )
