54d40c8bc1a2d4d0482f550a6e5607d7e3066f63
tonysnickers <antoine.mendy@hotmail.fr>
2022-10-26 16:43:20 +0200

    project 2 created

--- src/pages/Project2.js
+++ src/pages/Project2.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Buttons from '../components/Buttons'
+import Logo from '../components/Logo'
+import Mouse from '../components/Mouse'
+import Navigation from '../components/Navigation'
+import Project from '../components/Project'
 
 const Project2 = () => {
   return (
-    <div>Project2</div>
+    <main>
+        <Mouse/>
+      <div className="project">
+        <Navigation/>
+        <Logo/>
+        <Project projectNumber={1}/>
+        <Buttons left={'/project-1'} right={"/project-3"}/>
+      </div>
+    </main>
   )
 }
-
 export default Project2
